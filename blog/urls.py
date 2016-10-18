45dd6a6e583204a5467b5f1bb2cf56f1ec28d2a5
Beck <13524781846@139.com>
2016-10-18 11:31:57 +0800

    can view index,blogpost_detail page.

--- blog/urls.py
+++ blog/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^index/$','blogpost.views.index'),
+    url(r'^blog/(?P<slug>[^\.]+).html','blogpost.views.view_post',name='view_blog_post'),
 ]
